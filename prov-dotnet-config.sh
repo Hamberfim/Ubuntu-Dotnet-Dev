a10a771e277335510e12f13a4af75a8ec268c9fc
Hamberfim <adhamlingithub@gmail.com>
2024-05-20 12:22:19 -0500

    explicit sudo - hangs at ms-tools

--- prov-dotnet-config.sh
+++ prov-dotnet-config.sh
@@ -1,47 +1,52 @@
 #!/usr/bin/env bash
-echo "updating repo packages"
-apt-get update # get system up to date
+
+# Set DEBIAN_FRONTEND to noninteractive
+export DEBIAN_FRONTEND=noninteractive
+
+echo "######### updating repo packages"
+# adding sudo for Explicitness
+sudo apt-get update # get system up to date
 
 # Get needed packages are installed
-echo "installing needed packages"
-apt-get install git wget curl vim nano neovim openssh-client openssh-server build-essential
+echo "######### installing needed packages"
+sudo apt-get install git wget curl vim nano neovim openssh-client openssh-server build-essential
 
 # Download Microsoft signing key and repository
-echo "getting MS key and repo"
-wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
+echo "######### getting MS key and repo"
+sudo wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 
 # Install Microsoft signing key and repository
-echo "installing repo"
-dpkg -i packages-microsoft-prod.deb
+echo "######### installing repo"
+sudo dpkg -i packages-microsoft-prod.deb
 
 # Clean up
-echo "clean up"
-rm packages-microsoft-prod.deb
+echo "######### clean up"
+sudo rm packages-microsoft-prod.deb
 
 # Update packages list
-echo "updating repo packages"
-apt-get update
+echo "######### updating repo packages"
+sudo apt-get update
 
 ## Install .Net SDK
-echo "installing .Net 8 sdk"
-apt-get install dotnet-sdk-8.0
+echo "######### installing .Net 8 sdk"
+sudo apt-get install dotnet-sdk-8.0
 
 ## INSTALL MSSQL
 # download and register the MSSQL Server repository for Ubuntu
-echo "register mssql repo"
+echo "######### register mssql repo"
 curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | sudo tee /etc/apt/sources.list.d/mssql-server-2022.list
 
 # Update packages list
-echo "updating repo packages"
-apt-get update
+echo "######### updating repo packages"
+sudo apt-get update
 
 # install MSSQL
-echo "installing ms-sql"
-apt-get install -y mssql-server
+echo "######### installing ms-sql"
+sudo apt-get install -y mssql-server
 
 # Install MSSQL Tools
-echo "installing ms-sql tools"
-apt-get install -y mssql-tools18 unixodbc-dev
+echo "######### installing ms-sql tools"
+sudo apt-get install -y mssql-tools18 unixodbc-dev
 
 # Add to the env path in bash - output $PATH without expansion
 echo "exporting env paths"
