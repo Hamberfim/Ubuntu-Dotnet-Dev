329c7ebae56077e7bde138057dea47a2c2b10ad6
Hamberfim <adhamlingithub@gmail.com>
2024-05-20 13:23:38 -0500

    port forward and post config steps

--- prov-dotnet-config.sh
+++ prov-dotnet-config.sh
@@ -40,31 +40,9 @@ curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.
 echo "######### updating repo packages"
 sudo apt-get update
 
-# install MSSQL
+# Install MSSQL
 echo "######### installing ms-sql"
 sudo apt-get install -y mssql-server
 
-# # Install MSSQL Tools
-# echo "######### installing ms-sql tools"
-# sudo apt-get install -y mssql-tools18 unixodbc-dev
-
-# # Add to the env path in bash - output $PATH without expansion
-# echo "exporting env paths"
-# echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >>~/.bash_profile
-
-# # for interactive non-login session - output $PATH without expansion
-# echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >>~/.bashrc
-# # shellcheck source=/dev/null
-# source ~/.bashrc
-
-# ### Post install configuration required ###
-# #  vagrant ssh
-# #  sudo /opt/mssql/bin/mssql-conf setup
-# #  Lic selection 2
-# #  fill in the password
-# #  systemctl status mssql-server --no-pager
-# ############# NOTE #############
-# #  To connect remotely, you might need to open
-# #  the SQL Server TCP port (default 1433) on the firewall
-
-echo "finished installations"
+echo "######### finished installations #########"
+echo "######### Do post Vagrant config now #########"
