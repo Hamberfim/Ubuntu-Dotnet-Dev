de48e447d363093183c549983d2d9c28c45a64e3
Hamberfim <adhamlingithub@gmail.com>
2024-05-20 10:54:00 -0500

    initial setup -not ran

---
+++ prov-dotnet-config.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+apt-get update # get system up to date
+
+# Get needed packages are installed
+sudo apt install git wget curl vim nano neovim openssh-client openssh-server build-essential
+
+# Download Microsoft signing key and repository
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
+
+# Install Microsoft signing key and repository
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Clean up
+rm packages-microsoft-prod.deb
+
+# Update packages list
+sudo apt update
+
+## Install .Net SDK
+sudo apt install dotnet-sdk-8.0
+
+## INSTALL MSSQL
+# download and register the MSSQL Server repository for Ubuntu
+curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | sudo tee /etc/apt/sources.list.d/mssql-server-2022.list
+
+# Update packages list
+sudo apt update
+
+# install MSSQL
+sudo apt-get install -y mssql-server
+
+## Post install configuration required ##
+#  vagrant ssh
+#  sudo /opt/mssql/bin/mssql-conf setup
+#  Lic selection 2
+#  fill in the password
+#  systemctl status mssql-server --no-pager
+#  To connect remotely, you might need to open
+#  the SQL Server TCP port (default 1433) on the firewall
