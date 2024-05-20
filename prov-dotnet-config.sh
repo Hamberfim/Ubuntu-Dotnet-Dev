2466f95d4d784f88e2a7222853dd014743733ef1
Hamberfim <adhamlingithub@gmail.com>
2024-05-20 11:26:02 -0500

    switch to apt-get

--- prov-dotnet-config.sh
+++ prov-dotnet-config.sh
@@ -2,35 +2,35 @@
 apt-get update # get system up to date
 
 # Get needed packages are installed
-sudo apt install git wget curl vim nano neovim openssh-client openssh-server build-essential
+apt-get install git wget curl vim nano neovim openssh-client openssh-server build-essential
 
 # Download Microsoft signing key and repository
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
 
 # Install Microsoft signing key and repository
-sudo dpkg -i packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
 
 # Clean up
 rm packages-microsoft-prod.deb
 
 # Update packages list
-sudo apt update
+apt-get update
 
 ## Install .Net SDK
-sudo apt install dotnet-sdk-8.0
+apt-get install dotnet-sdk-8.0
 
 ## INSTALL MSSQL
 # download and register the MSSQL Server repository for Ubuntu
 curl -fsSL https://packages.microsoft.com/config/ubuntu/22.04/mssql-server-2022.list | sudo tee /etc/apt/sources.list.d/mssql-server-2022.list
 
 # Update packages list
-sudo apt update
+apt-get update
 
 # install MSSQL
-sudo apt install -y mssql-server
+apt-get install -y mssql-server
 
 # Install MSSQL Tools
-sudo apt install mssql-tools18 unixodbc-dev
+apt-get install -y mssql-tools18 unixodbc-dev
 
 # Add to the env path in bash - output $PATH without expansion
 echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >>~/.bash_profile
